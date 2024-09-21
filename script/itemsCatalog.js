e62341931785985da5e877e34e3771aeb12ab6e5
Mother1teresa <129117416+Mother1teresa@users.noreply.github.com>
2024-09-21 20:26:50 +0300

    товары + Пагинация

---
+++ script/itemsCatalog.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".heart").forEach(function (element) {
+  element.addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+});
+document.querySelectorAll(".btn-blue-basket").forEach(function (button) {
+  button.addEventListener("click", function () {
+    this.classList.toggle("active");
+  });
+});
