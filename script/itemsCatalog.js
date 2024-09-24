cb0ecc06b1c6433bb0907a97a4b6cfc7c6c0f155
Mother1teresa <129117416+Mother1teresa@users.noreply.github.com>
2024-09-24 18:20:19 +0300

    mob catalog + filters

--- script/itemsCatalog.js
+++ script/itemsCatalog.js
@@ -32,3 +32,29 @@ btnBasket.forEach((bottom) => {
     }
   });
 });
+const categoriesButton = document.querySelector('.main-categories_title');
+const categoriesContent = document.querySelector('.box-white');
+const backArrow = document.querySelector('.back-arrow');
+
+categoriesButton.addEventListener('click', () => {
+  categoriesContent.classList.toggle('active');
+});
+
+backArrow.addEventListener('click', () => {
+  categoriesButton.classList.remove('active');
+  categoriesContent.classList.remove('active');
+});
+
+
+
+const filtersButton = document.querySelector('.filters-title');
+const filtersContent = document.querySelector('.box_filters');
+const backArrows = document.querySelector('.back-arrows');
+
+filtersButton.addEventListener('click', () => {
+  filtersContent.classList.toggle('active');
+});
+
+backArrows.addEventListener('click', () => {
+  filtersContent.classList.remove('active');
+});
